89bae33e027586b44de87f60b7aa888feeca08dd
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2025-04-02 03:20:37 +0200

    fixed!
    > debounceDelay value (currently 50, tune if needed)
    > clockwise/counter-clockwise logic within the onPulsed function ( swap if the direction is inverted for specific encoder. )

--- crank.ts
+++ crank.ts
@@ -1,110 +1,59 @@
-let lastS1State = 0;
-let s1State = 0;
-let s2State = 0;
-let keyState = 0;
-let lastKeyState = 0;
-let lastMovementTime = 0;
-let debounceDelay = 5;
-let dotX = 0;
-let dotY = 0;
-const MODE_TWO_WAY = 0;
-const MODE_FORWARD = 1;
-const MODE_REWIND = 2;
-let currentMode = MODE_TWO_WAY;
+let dotX = 2;
+let dotY = 2;
+
+let lastMovementTime: number = 0;
+const debounceDelay: number = 50;
+
+let lastKeyState: number = 1;
 
 pins.setPull(DigitalPin.P0, PinPullMode.PullUp);
 pins.setPull(DigitalPin.P1, PinPullMode.PullUp);
 pins.setPull(DigitalPin.P2, PinPullMode.PullUp);
-
 keyboard.startKeyboardService()
-
-led.plot(2, 2)
-basic.pause(500)
-basic.clearScreen()
 led.plot(dotX, dotY)
-lastS1State = pins.digitalReadPin(DigitalPin.P0);
 
-function showModeIndicator() {
-    let savedX = dotX;
-    let savedY = dotY;
-    basic.clearScreen()
-    
-    if (currentMode === MODE_TWO_WAY) {
-        led.plot(2, 2)
-    } else if (currentMode === MODE_FORWARD) {
-        led.plot(4, 2)
-    } else if (currentMode === MODE_REWIND) {
-        led.plot(0, 2)
-    }
-    
-    basic.pause(400)
-    basic.clearScreen()
-    led.plot(savedX, savedY)
-}
-
-input.onButtonPressed(Button.A, function() {
-    currentMode = MODE_REWIND;
-    showModeIndicator();
-})
-
-input.onButtonPressed(Button.B, function() {
-    currentMode = MODE_FORWARD;
-    showModeIndicator();
-})
-
-input.onButtonPressed(Button.AB, function() {
-    currentMode = MODE_TWO_WAY;
-    showModeIndicator();
-})
-
-basic.forever(function () {
-    s1State = pins.digitalReadPin(DigitalPin.P0);
-    s2State = pins.digitalReadPin(DigitalPin.P1);
-    keyState = pins.digitalReadPin(DigitalPin.P2);
-    
-    if (s1State !== lastS1State && input.runningTime() - lastMovementTime > debounceDelay) {
-        lastMovementTime = input.runningTime();
-        let isClockwise = (s2State !== s1State);
+pins.onPulsed(DigitalPin.P0, PulseValue.Low, function () {
+    let now = input.runningTime();
+    if (now - lastMovementTime > debounceDelay) {
+        lastMovementTime = now;
+        let dtState = pins.digitalReadPin(DigitalPin.P1);
         led.unplot(dotX, dotY);
-        
-        if ((isClockwise && currentMode !== MODE_REWIND) || (!isClockwise && currentMode === MODE_FORWARD)) {
-            dotX++;
-            if (dotX > 4) {
-                dotX = 0;
-                dotY++;
-                if (dotY > 4) {
-                    dotY = 0;
-                }
-            }
-            keyboard.sendString(keyboard.keys(keyboard._Key.right))
-        } 
-        else if ((!isClockwise && currentMode !== MODE_FORWARD) || (isClockwise && currentMode === MODE_REWIND)) {
-            dotX--;
+
+        if (dtState == 0) { // Clockwise Example (adjust if reversed)
+            dotX = (dotX + 1) % 5;
+            if (dotX == 0) dotY = (dotY + 1) % 5;
+            keyboard.sendString(keyboard.keys(keyboard._Key.right));
+        } else { // Counter-Clockwise Example (adjust if reversed)
+            dotX = (dotX - 1);
             if (dotX < 0) {
                 dotX = 4;
-                dotY--;
-                if (dotY < 0) {
-                    dotY = 4;
-                }
+                dotY = (dotY - 1);
+                if (dotY < 0) dotY = 4;
             }
-            keyboard.sendString(keyboard.keys(keyboard._Key.left))
+            keyboard.sendString(keyboard.keys(keyboard._Key.left));
         }
-        
         led.plot(dotX, dotY);
     }
-    
-    lastS1State = s1State;
-    
+});
+
+basic.forever(function () {
+    let keyState = pins.digitalReadPin(DigitalPin.P2);
+
     if (keyState === 0 && lastKeyState === 1) {
-        led.unplot(dotX, dotY);
-        dotX = 0;
-        dotY = 0;
-        led.plot(dotX, dotY);
-        keyboard.sendString(keyboard.keys(keyboard._Key.enter))
-        led.unplot(dotX, dotY);
-        basic.pause(100);
-        led.plot(dotX, dotY);
+        basic.pause(30); // Switch debounce
+        keyState = pins.digitalReadPin(DigitalPin.P2);
+        if (keyState === 0) {
+            led.unplot(dotX, dotY);
+            dotX = 0;
+            dotY = 0;
+            led.plot(dotX, dotY);
+            keyboard.sendString(keyboard.keys(keyboard._Key.enter))
+            basic.pause(100);
+            led.unplot(dotX, dotY);
+            basic.pause(50);
+            led.plot(dotX, dotY);
+        }
     }
-    
     lastKeyState = keyState;
+    basic.pause(20);
 })
